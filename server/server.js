b645c3216d309ca178ad026132658381249950c6
Wiezien <145956394+Wiezien@users.noreply.github.com>
2024-07-16 05:58:11 +0100

    seeding part 2

--- server/server.js
+++ server/server.js
@@ -81,4 +81,30 @@ const seedData = [
     ],
     rating: 3.8,
   },
+  {
+    name: "Vegetarian Haven",
+    image:
+      "https://media.geeksforgeeks.org/wp-content/uploads/20240110004602/pexels-chan-walrus-958545-(1).jpg",
+    menu: [
+      {
+        name: "Quinoa Salad",
+        price: 8,
+        image:
+          "https://media.geeksforgeeks.org/wp-content/uploads/20240110004602/pexels-chan-walrus-958545-(1).jpg",
+      },
+      {
+        name: "Eggplant Parmesan",
+        price: 12,
+        image:
+          "https://media.geeksforgeeks.org/wp-content/uploads/20240110004602/pexels-chan-walrus-958545-(1).jpg",
+      },
+      {
+        name: "Mushroom Risotto",
+        price: 16,
+        image:
+          "https://media.geeksforgeeks.org/wp-content/uploads/20240110004602/pexels-chan-walrus-958545-(1).jpg",
+      },
+    ],
+    rating: 4.2,
+  },
 ];
