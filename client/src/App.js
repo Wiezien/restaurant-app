18c22464a46d1789d8970b16f2422170488f7a09
Wiezien <145956394+Wiezien@users.noreply.github.com>
2024-07-19 05:17:51 +0100

    app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,22 @@
+import React, { useContext } from "react";
+import RestaurantList from "./components/RestaurantList";
+import DishesMenu from "./components/DishesMenu";
+import Cart from "./components/Cart";
+import { RestaurantContext } from "./contexts/RestaurantContext";
 import "./App.css";
 
-function App() {}
+const App = () => {
+  const { selectedRestaurant } = useContext(RestaurantContext);
+
+  return (
+    <>
+      <div className="container">
+        <h1 className="header">Restaurant App</h1>
+        <Cart style={{ position: "absolute", right: "20px", top: "20px" }} />
+        <RestaurantList />
+        {selectedRestaurant && <DishesMenu />}
+      </div>
+    </>
+  );
+};
 export default App;
