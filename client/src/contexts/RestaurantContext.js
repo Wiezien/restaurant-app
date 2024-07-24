597d4efb321984c7305f659721b81c15abe1ee13
Wiezien <145956394+Wiezien@users.noreply.github.com>
2024-07-24 05:40:02 +0100

    return

--- client/src/contexts/RestaurantContext.js
+++ client/src/contexts/RestaurantContext.js
@@ -24,66 +24,77 @@ const RestaurantProvider = ({ children }) => {
 
   const handleAddItems = (dish) => {
     console.log("Dish:", dish);
-  };
 
-  const existingItemIndex = cartItems.findIndex(
-    (item) => item._id === dish._id
-  );
-
-  if (existingItemIndex !== -1) {
-    console.log(
-      `Dish already exists in the cart. 
-        You may want to update the quantity.`
-    );
-    const updatedCartItems = [...cartItems];
-    updatedCartItems[existingItemIndex] = {
-      ...updatedCartItems[existingItemIndex],
-      quantity: updatedCartItems[existingItemIndex].quantity + 1,
-    };
-
-    setCartItems(updatedCartItems);
-  } else {
-    console.log("Dish does not exist in the cart. Adding to the cart.");
-    console.log("cart", cartItems.length);
-
-    setCartItems([...cartItems, { ...dish, quantity: 1 }]);
-  }
-  setTotalPrice((prev) => prev + dish.price);
-};
-
-const handleRemoveItems = (dish) => {
-  console.log("Dish ID to remove:", dish);
-
-  const existingItemIndex = cartItems.findIndex(
-    (item) => item._id === dish._id
-  );
-  if (existingItemIndex !== -1) {
-    console.log(
-      `Dish exists in the cart. You may want 
-         to decrease the quantity or remove it.`
+    const existingItemIndex = cartItems.findIndex(
+      (item) => item._id === dish._id
     );
 
-    const updatedCartItems = [...cartItems];
-    if (updatedCartItems[existingItemIndex].quantity > 1) {
+    if (existingItemIndex !== -1) {
+      console.log(
+        `Dish already exists in the cart. 
+				You may want to update the quantity.`
+      );
+      const updatedCartItems = [...cartItems];
       updatedCartItems[existingItemIndex] = {
         ...updatedCartItems[existingItemIndex],
-        quantity: updatedCartItems[existingItemIndex].quantity - 1,
+        quantity: updatedCartItems[existingItemIndex].quantity + 1,
       };
+
+      setCartItems(updatedCartItems);
     } else {
-      updatedCartItems.splice(existingItemIndex, 1);
+      console.log("Dish does not exist in the cart. Adding to the cart.");
+      console.log("cart", cartItems.length);
+
+      setCartItems([...cartItems, { ...dish, quantity: 1 }]);
     }
-    setCartItems(updatedCartItems);
-    setTotalPrice((prev) => prev - dish.price);
-  } else {
-    console.log("Dish does not exist in the cart.");
-  }
+    setTotalPrice((prev) => prev + dish.price);
+  };
+
+  const handleRemoveItems = (dish) => {
+    console.log("Dish ID to remove:", dish);
+
+    const existingItemIndex = cartItems.findIndex(
+      (item) => item._id === dish._id
+    );
+
+    if (existingItemIndex !== -1) {
+      console.log(
+        `Dish exists in the cart. You may want 
+				to decrease the quantity or remove it.`
+      );
+
+      const updatedCartItems = [...cartItems];
+
+      if (updatedCartItems[existingItemIndex].quantity > 1) {
+        updatedCartItems[existingItemIndex] = {
+          ...updatedCartItems[existingItemIndex],
+          quantity: updatedCartItems[existingItemIndex].quantity - 1,
+        };
+      } else {
+        updatedCartItems.splice(existingItemIndex, 1);
+      }
+
+      setCartItems(updatedCartItems);
+      setTotalPrice((prev) => prev - dish.price);
+    } else {
+      console.log("Dish does not exist in the cart.");
+    }
+  };
+
+  const value = {
+    restaurants,
+    selectedRestaurant,
+    setSelectedRestaurant,
+    handleAddItems,
+    handleRemoveItems,
+    totalPrice,
+  };
+
+  return (
+    <RestaurantContext.Provider value={value}>
+      {children}
+    </RestaurantContext.Provider>
+  );
 };
 
-const value = {
-  restaurants,
-  selectedRestaurant,
-  setSelectedRestaurant,
-  handleAddItems,
-  handleRemoveItems,
-  totalPrice,
-};
+export { RestaurantContext, RestaurantProvider };
