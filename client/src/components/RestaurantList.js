4c7f647a12aba3bea01d40f5be53eae8de28d070
Wiezien <145956394+Wiezien@users.noreply.github.com>
2024-07-25 05:17:16 +0100

    useEffect

--- client/src/components/RestaurantList.js
+++ client/src/components/RestaurantList.js
@@ -6,3 +6,12 @@ const RestaurantList = () => {
   const [ratingFilter, setRatingFilter] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
 };
+useEffect(() => {
+  filterRestaurants();
+}, [ratingFilter, searchTerm, restaurants]);
+
+const handleRestaurantClick = (restaurantId) => {
+  setSelectedRestaurant(
+    restaurants.find((restaurant) => restaurant._id === restaurantId)
+  );
+};
