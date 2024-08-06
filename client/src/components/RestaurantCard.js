1e8ded6902cb6a4cf482ba0136f76a3c5e64c2f1
Wiezien <145956394+Wiezien@users.noreply.github.com>
2024-08-06 06:36:25 +0100

    styled restaurant-image

--- client/src/components/RestaurantCard.js
+++ client/src/components/RestaurantCard.js
@@ -5,7 +5,11 @@ const RestaurantCard = ({ restaurant, onClick }) => {
     <div className="card" onClick={onClick}>
       <h3>{restaurant.name}</h3>
       <div className="image-container">
-        <img src={restaurant.image} alt={restaurant.name} />
+        <img
+          className="restaurant-image"
+          src={restaurant.image}
+          alt={restaurant.name}
+        />
       </div>
       <p>Rating: {restaurant.rating}</p>
     </div>
