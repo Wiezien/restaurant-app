0ec5b99ea1447dc14953dee63805808b28978ae7
Wiezien <145956394+Wiezien@users.noreply.github.com>
2024-08-06 06:25:20 +0100

    styled restaurant card

--- client/src/components/RestaurantCard.js
+++ client/src/components/RestaurantCard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const RestaurantCard = ({ restaurant, onClick }) => {
   return (
-    <div onClick={onClick}>
+    <div className="card" onClick={onClick}>
       <h3>{restaurant.name}</h3>
       <div>
         <img src={restaurant.image} alt={restaurant.name} />
